613f1c58611b1448c523236a2d44cab869577edd
= <=>
2026-06-13 13:06:33 +0530

    posts by category

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,18 @@
 
 from django.shortcuts import render
+from blogs.models import Category,Blog
 
 def home(request):
-    return render(request,'home.html')
+    categories = Category.objects.all()
+    print(categories)
+    featured_posts = Blog.objects.filter(is_featured = True,status = 'Published').order_by('updated_at')
+    print(featured_posts)
+    posts = Blog.objects.filter(is_featured = False,status='Published')
+    print(posts)
+    context ={
+        'categories': categories,
+        'featured_posts':featured_posts,
+        'posts': posts,
+    }
+    #change in home.html
+    return render(request,'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404, render
+from .models import *
 
 # Create your views here.
+def posts_by_category(request,category_id):
+    print(category_id)
+    #fetch the posts that belongs to the category with the given id
+    posts = Blog.objects.filter(status = 'Published',category = category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context ={
+        'posts': posts,
+        'category': category,
+    }
+    return render(request,'posts_by_category.html',context)
