5877dc4d1d347b3f84758cc5eb19ab26d965574e
= <=>
2026-06-14 11:41:54 +0530

    fix build.sh and create superuser

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword123')" | python manage.py shell
