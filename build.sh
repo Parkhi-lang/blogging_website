c8fafa114f8edfb8761ec99536f3d3e557f10c73
= <=>
2026-06-14 13:10:37 +0530

    debug superuser

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
+#!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
+print('All users:', list(User.objects.values('username', 'is_staff', 'is_superuser')))
 u, created = User.objects.get_or_create(username='admin')
 u.set_password('admin1234')
 u.is_staff = True
 u.is_superuser = True
+u.is_active = True
 u.save()
-print('Done')
+print('Created:', created)
+print('User saved:', u.username, u.is_staff, u.is_superuser, u.is_active)
 "
