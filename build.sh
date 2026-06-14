079d675695786a14e7d097cfdd9d45a77502145c
= <=>
2026-06-14 13:07:16 +0530

    reset superuser password

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env bash
-set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword123')
-    print('Superuser created')
-else:
-    u = User.objects.get(username='admin')
-    u.set_password('yourpassword123')
-    u.save()
-    print('Password reset')
+u, created = User.objects.get_or_create(username='admin')
+u.set_password('admin1234')
+u.is_staff = True
+u.is_superuser = True
+u.save()
+print('Done')
 "
